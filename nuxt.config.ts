60c485a68ca01122e7759f4e9d1cc90eb1e58811
Damilola Yekinni <81156000+psamuel112@users.noreply.github.com>
2026-01-28 03:29:44 +0100

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,26 @@
 import { defineNuxtConfig } from "nuxt/config";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    runtimeConfig: {
+  css: ['~/assets/css/tailwind.css'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
+
+  devtools: { enabled: true },
+
+  ssr: false, // disable SSR, fully client-side static site
+
+  nitro: {
+    preset: 'static' // generate static output for deployment
+  },
+
+  runtimeConfig: {
     public: {
       TMDB_API_KEY: process.env.TMDB_API_KEY,
       TMDB_BASE_URL: process.env.TMDB_BASE_URL
     }
   },
-  compatibilityDate: '2025-07-15',
-css: ['~/assets/css/tailwind.css'],
-  modules: ['@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-    
-  ],
-  
-  devtools: { enabled: true }
+
+  compatibilityDate: '2025-07-15'
 })
